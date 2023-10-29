d759fd0bfb9e58a33990ac5957d3c4ccbe6fab6a
TactoDaDev <jj@tacto.ca>
2023-10-28 18:50:18 -0600

    .

--- [Main]/bcc-minigames/ui/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-	baseURL: `https://${typeof GetParentResourceName !== 'undefined' ? GetParentResourceName() : 'bcc-minigames'}/`,
-});
-
-export default api;
